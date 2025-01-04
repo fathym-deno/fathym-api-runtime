a6129c86ef673f4fd50dcbc5f9ee56e3bb458829
mcgear <michael.gearhardt@gmail.com>
2025-01-03 22:34:47 -0700

    more actuators

--- src/plugins/RuntimePlugin.ts
+++ src/plugins/RuntimePlugin.ts
@@ -1,10 +1,27 @@
 import { EverythingAsCode } from '@fathym/eac';
 import { EaCRuntimeConfig } from '@fathym/eac/runtime/config';
-import { EaCRuntimePlugin, EaCRuntimePluginConfig } from '@fathym/eac/runtime/plugins';
+import {
+  EaCRuntimePlugin,
+  EaCRuntimePluginConfig,
+} from '@fathym/eac/runtime/plugins';
 import { EverythingAsCodeApplications } from '@fathym/eac-applications';
 import { EaCStewardPlugin } from '@fathym/eac-applications/steward/plugins';
 import { EaCAPIProcessor } from '@fathym/eac-applications/processors';
+import {
+  EaCAzureAPIPlugin,
+  EaCAzureStewardPlugin,
+} from '@fathym/eac-azure/steward/plugins';
 import { EaCLocalDistributedFileSystemDetails } from '@fathym/eac-dfs';
+import { EaCIoTStewardPlugin } from '@fathym/eac-iot/steward/plugins';
+import {
+  EaCLicensingAPIPlugin,
+  EaCLicensingStewardPlugin,
+} from '@fathym/eac-licensing/steward/plugins';
+import {
+  EaCSourceConnectionsStewardPlugin,
+  EaCSourcesStewardPlugin,
+  EaCDevOpsActionsStewardPlugin,
+} from '@fathym/eac-sources/steward/plugins';
 import { IoCContainer } from '@fathym/ioc';
 
 export default class RuntimePlugin implements EaCRuntimePlugin {
@@ -19,18 +36,57 @@ export default class RuntimePlugin implements EaCRuntimePlugin {
         new EaCStewardPlugin({
           Application: {
             Path: '/api/eac*',
+            Priority: 300,
           },
         }),
-        // new EaCAzureStewardPlugin({
-        //   Application: {
-        //     Path: '/api/eac*',
-        //   },
-        // }),
-        // new EaCGitHubStewardPlugin({
-        //   Application: {
-        //     Path: '/api/eac*',
-        //   },
-        // }),
+        new EaCAzureAPIPlugin({
+          Application: {
+            Path: '/api/eac/azure*',
+            Priority: 500,
+          },
+        }),
+        new EaCAzureStewardPlugin({
+          Application: {
+            Path: '/api/steward/azure*',
+            Priority: 500,
+          },
+        }),
+        new EaCIoTStewardPlugin({
+          Application: {
+            Path: '/api/steward/iot*',
+            Priority: 500,
+          },
+        }),
+        new EaCSourceConnectionsStewardPlugin({
+          Application: {
+            Path: '/api/steward/source-connections*',
+            Priority: 500,
+          },
+        }),
+        new EaCSourcesStewardPlugin({
+          Application: {
+            Path: '/api/steward/sources*',
+            Priority: 500,
+          },
+        }),
+        new EaCDevOpsActionsStewardPlugin({
+          Application: {
+            Path: '/api/steward/devops-actions*',
+            Priority: 500,
+          },
+        }),
+        new EaCLicensingAPIPlugin({
+          Application: {
+            Path: '/api/steward/devops-actions*',
+            Priority: 500,
+          },
+        }),
+        new EaCLicensingStewardPlugin({
+          Application: {
+            Path: '/api/steward/devops-actions*',
+            Priority: 500,
+          },
+        }),
       ],
       IoC: new IoCContainer(),
       EaC: {
